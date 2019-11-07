c4e127bcb64bc830089e0a4d4a74fdc83968a7f9
youth2ee <sprirealee@gmail.com>
2019-11-07 17:16:41 +0900

    191107

--- src/test/java/com/naver/s1/DataSourceTest.java
+++ src/test/java/com/naver/s1/DataSourceTest.java
@@ -7,16 +7,17 @@ import java.sql.SQLException;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 public class DataSourceTest extends TestAbstractCase {
 
 	@Inject
-	private DataSource dataSource;
+	private SqlSession sqlSession;
 	
 	@Test
 	public void test() throws Exception {
-		assertNotNull(dataSource.getConnection());
+		assertNotNull(sqlSession);
 	}
 
 }
